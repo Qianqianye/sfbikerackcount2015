6785004a59b773d0118f23a233ba78d3dbfe4cb2
Qianqianye <qianqian.ye44@gmail.com>
2016-09-07 01:25:45 -0700

    add time slider
    time slider range, new css layout, new basemap

--- sketch.js
+++ sketch.js
@@ -1,91 +1,133 @@
 
+var heatmapLayer ;
+var globalDataArray = [];
+
 function setup(){
 	loadJSON("data.json",gotData);
-
 }
 
 function gotData (data){
+  var newArray = [];
+  for (var i = 0; i < data.data.length; i+=1) {
+    println('----- index ' + i + ' -----');
 
+    var nowItem = data.data[i]
+    var yearN = nowItem[8];
+    var monthN = nowItem[9];
+    var racknumberN = nowItem[15];
+    var locationN = nowItem[18];
+    var latN = locationN[1];
+    var lonN = locationN[2];
 
-var newArray = [];
-
-for (var i = 0; i < data.data.length; i+=1) {
-// for (var i = 0; i < 100; i+=1) {	
-
-	println('----- index ' + i + ' -----');
-		
-	var nowItem = data.data[i]
-
-	var yearN = nowItem[8];
-	var monthN = nowItem[9];
-	var racknumberN = nowItem[15];
-	var locationN = nowItem[18];
-	var latN = locationN[1];
-	var lonN = locationN[2];
-
-	// println(nowItem)
-
-	// println(yearN);
-	// println(monthN);
-	// println(racknumberN);
     println(latN);
-     println(lonN);
+    println(lonN);
+    if(latN != null && lonN != null){
+      newArray.push({'lat':latN, 'lng':lonN, 'count':racknumberN, 'month': monthN, 'year': yearN });
+    }
+  }
 
-
-if(latN != null && lonN != null){
-		newArray.push({'lat':latN, 'lng':lonN, 'count':racknumberN});
+  view();
+  globalDataArray = newArray;
 }
 
-};
+function setData(arrayIn){
+  var testData = {
+    max: 20,
+    data: arrayIn
+  };
+  heatmapLayer.setData(testData);
+}
 
-var testData = {
-          max: 20,
-          data: newArray
-      };
-
-   var baseLayer = L.tileLayer(
-          'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-            maxZoom: 18
-          }
-        );
-
-        var cfg = {
-          // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-          "radius": .002,
-          "maxOpacity": .8, 
-          // scales the radius based on map zoom
-          "scaleRadius": true, 
-          // if set to false the heatmap uses the global maximum for colorization
-          // if activated: uses the data maximum within the current map boundaries 
-          //   (there will always be a red spot with useLocalExtremas true)
-          "useLocalExtrema": true,
-          // which field name in your data represents the latitude - default "lat"
-          latField: 'lat',
-          // which field name in your data represents the longitude - default "lng"
-          lngField: 'lng',
-          // which field name in your data represents the data value - default "value"
-          valueField: 'count'
-        };
+function view(){
 
 
-        var heatmapLayer = new HeatmapOverlay(cfg);
+ 
+// option 1 Black and white
+// var baseLayer = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}',{
+//     subdomains: 'abcd',
+//     maxZoom: 18,
+//     ext: 'png',
+//   });
 
-        var map = new L.Map('map', {
-          center: new L.LatLng(37.7735759476, -122.429860009),
-          zoom: 13,
+// option 2 Darkmatter
+var baseLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',{
+    subdomains: 'abcd',
+    maxZoom: 18,
+  });
+
+  var cfg = {
+            // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+            "radius": .0018,
+            "maxOpacity": .75, 
+            // scales the radius based on map zoom
+            "scaleRadius": true, 
+            // if set to false the heatmap uses the global maximum for colorization
+            // if activated: uses the data maximum within the current map boundaries 
+            //   (there will always be a red spot with useLocalExtremas true)
+            "useLocalExtrema": true,
+            // which field name in your data represents the latitude - default "lat"
+            latField: 'lat',
+            // which field name in your data represents the longitude - default "lng"
+            lngField: 'lng',
+            // which field name in your data represents the data value - default "value"
+            valueField: 'count'
+          };
+
+
+          heatmapLayer = new HeatmapOverlay(cfg);
+
+          var map = new L.Map('map', {
+            center: new L.LatLng(37.7735759476, -122.429860009),
+            zoom: 13,
+            fullscreenControl: true,
+            timeDimension: true,
+            timeDimensionOptions: {
+            timeInterval: "1997-09/2015-12",
+            period: "P1M",
+           },
+           timeDimensionControl: true,
+           timeDimensionControlOptions: {
+           autoPlay: true,
+           loopButton: true,
+           timeSteps: 1,
+           playReverseButton: true,
+           limitSliders: true,
+           playerOptions: {
+           buffer: 0,
+           transitionTime: 250,
+           loop: true,
+            }
+          },
           layers: [baseLayer, heatmapLayer]
         });
 
-        heatmapLayer.setData(testData);
+          map.timeDimension.on('timeload', newTimeLoad);
+          layer = heatmapLayer;
+        }
 
-        // make accessible for debugging
-        layer = heatmapLayer;
+        function newTimeLoad(e){
+          timeNow = e.time;
+          var d = new Date(timeNow);
+          var Dyear;
+          var Dmonth;
+          Dyear = d.getFullYear();
+          Dmonth = d.getMonth();
 
-}
+       var newTimeArray = []; // start off with a blank array
 
+       for (var i = 0; i < globalDataArray.length; i++) {
+         var dataMonth = globalDataArray[i].month;
+         var dataYear = globalDataArray[i].year;
 
-function draw(){
-}	
+         if(dataYear <= Dyear){
+           if(dataYear == Dyear && dataMonth <= Dmonth){
+             newTimeArray.push({'lat':globalDataArray[i].lat, 'lng':globalDataArray[i].lng, 'count':globalDataArray[i].count });
+           }
+           else if(dataYear < Dyear){
+             newTimeArray.push({'lat':globalDataArray[i].lat, 'lng':globalDataArray[i].lng, 'count':globalDataArray[i].count });
+           }
+         }
+       };
 
-
+       setData(newTimeArray);
+     }
